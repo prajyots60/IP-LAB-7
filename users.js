a3df37b7af56412295526f81e488e6c762a636ac
Prajyot Shinde <prajyotshinde60@gmail.com>
2024-10-19 12:59:58 +0530

    Update user language to Marathi

--- users.js
+++ users.js
@@ -1,7 +1,7 @@
 const users =[
   {
-  "name": "Adeel Solangi",
-  "language": "Sindhi",
+  "name": "Prajyot Shinde",
+  "language": "Marathi",
   "id": "V59OF92YF627HFY0",
   "bio": "Donec lobortis eleifend condimentum. Cras dictum dolor lacinia lectus vehicula rutrum. Maecenas quis nisi nunc. Nam tristique feugiat est vitae mollis. Maecenas quis nisi nunc.",
   "version": 6.1
